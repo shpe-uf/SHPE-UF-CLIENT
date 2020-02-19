072f2d9cf9ead403b201222ddf194e01c22a36af
juansuh <juansuhr@gmail.com>
2020-02-19 16:15:08 -0500

    fixed provider

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import { setContext } from "apollo-link-context";
 let port = process.env.PORT || 5000;
 
 const httpLink = createHttpLink({
-  uri: "https://apollo-shpe-uf.herokuapp.com/"
+  uri: "https://shpe-uf.herokuapp.com/"
 });
 
 const authLink = setContext(() => {
