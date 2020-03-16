729b54cd2ecb5e511ceacd9c280ef6828f07084f
despantoso@ufl.edu <despantoso@ufl.edu>
2020-03-16 16:45:00 -0400

    Dropdown Menu created with consoling what is clicked.

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -6,12 +6,12 @@ function DropdownMenu({ title }) {
   return (    
     <Dropdown inline text={title = "Major"}>
       <Dropdown.Menu>
-          <Dropdown.Item text="Major"/>
-          <Dropdown.Item text="Year"/>
-          <Dropdown.Item text="Country of Origin"/>
-          <Dropdown.Item text="Sex"/>
-          <Dropdown.Item text="Ethnicity"/>
-          <Dropdown.Item text="Class Sharing"/>
+          <Dropdown.Item text="Major" onClick={() => console.log("Clicking MAJOR")}/>
+          <Dropdown.Item text="Year" onClick={() => console.log("Clicking YEAR")}/>
+          <Dropdown.Item text="Country of Origin" onClick={() => console.log("Clicking COUNTRY OF ORIGIN")}/>
+          <Dropdown.Item text="Sex" onClick={() => console.log("Clicking SEX")}/>
+          <Dropdown.Item text="Ethnicity" onClick={() => console.log("Clicking ETHNICITY")}/>
+          <Dropdown.Item text="Class Sharing" onClick={() => console.log("Clicking CLASS SHARING")}/>
       </Dropdown.Menu>
     </Dropdown>
   );
