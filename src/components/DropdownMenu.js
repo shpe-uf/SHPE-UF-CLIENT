2e3954a7a8ab95033ccb52537530c9bbcb5d169c
despantoso@ufl.edu <despantoso@ufl.edu>
2020-03-22 19:39:27 -0400

    Added options and text to options so dropdown category is modified

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -3,16 +3,14 @@ import { Dropdown, Menu } from "semantic-ui-react";
 
 function DropdownMenu() {
   var [title, setTitle] = useState("Major");
-  var searchTitle = "Search " + title;
-  //console.log(title);
-
-  const majorOptions = [
+  var [options, setOptions] = useState([
     { text: "Computer Science" },
-    { text: "Agriculture Studies" },
-    { text: "Forestry Management" },
+    { text: "Computer Engineering" },
+    { text: "Digital Arts and Sciences" },
     { text: "Biomedical Engineering" }
-  ];
-
+  ]);
+  var searchTitle = "Search " + title;
+  
   return (
     <Menu secondary>
       <Menu.Item>
@@ -22,36 +20,55 @@ function DropdownMenu() {
               text="Major"
               onClick={() => {
                 setTitle((title = "Major"));
+                setOptions([
+                  { text: "Computer Science" },
+                  { text: "Computer Engineering" },
+                  { text: "Digital Arts and Sciences" },
+                  { text: "Biomedical Engineering" }
+                ]);
               }}
             />
             <Dropdown.Item
               text="Year"
               onClick={() => {
                 setTitle((title = "Year"));
+                setOptions([
+                  { text: "1st Year" },
+                  { text: "2nd Year" }]);
               }}
             />
             <Dropdown.Item
               text="Country of Origin"
               onClick={() => {
                 setTitle((title = "Country of Origin"));
+                setOptions([
+                  { text: "Cuba" },
+                  { text: "Venezuela" }]);
               }}
             />
             <Dropdown.Item
               text="Sex"
               onClick={() => {
                 setTitle((title = "Sex"));
+                setOptions([
+                  { text: "Female" }, 
+                  { text: "Male" }]);
               }}
             />
             <Dropdown.Item
               text="Ethnicity"
               onClick={() => {
                 setTitle((title = "Ethnicity"));
+                setOptions([
+                  { text: "Hispanic/Latino" }]);
               }}
             />
             <Dropdown.Item
               text="Class Sharing"
               onClick={() => {
                 setTitle((title = "Class Sharing"));
+                setOptions([
+                  { text: "COP4600" }]);
               }}
             />
           </Dropdown.Menu>
@@ -64,7 +81,8 @@ function DropdownMenu() {
             placeholder={searchTitle}
             item
             search
-            options={majorOptions}
+            options={options}
+            onClick={() => console.log("SET FILTER")}
           />
         </Menu.Item>
       </Menu.Menu>
