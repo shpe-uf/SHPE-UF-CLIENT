b5ed1b753e8ac915525b081003f37ace6b28bba4
despantoso@ufl.edu <despantoso@ufl.edu>
2020-03-22 19:16:43 -0400

    Menu on the left and in the right. Left is for category and right is for searching in category

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -6,53 +6,67 @@ function DropdownMenu() {
   var searchTitle = "Search " + title;
   //console.log(title);
 
-  return (
-    <Menu>
-      <Dropdown item text={title}>
-        <Dropdown.Menu>
-          <Dropdown.Item
-            text="Major"
-            onClick={() => {
-              setTitle((title = "Major"));
-            }}
-          />
-          <Dropdown.Item
-            text="Year"
-            onClick={() => {
-              setTitle((title = "Year"));
-            }}
-          />
-          <Dropdown.Item
-            text="Country of Origin"
-            onClick={() => {
-              setTitle((title = "Country of Origin"));
-            }}
-          />
-          <Dropdown.Item
-            text="Sex"
-            onClick={() => {
-              setTitle((title = "Sex"));
-            }}
-          />
-          <Dropdown.Item
-            text="Ethnicity"
-            onClick={() => {
-              setTitle((title = "Ethnicity"));
-            }}
-          />
-          <Dropdown.Item
-            text="Class Sharing"
-            onClick={() => {
-              setTitle((title = "Class Sharing"));
-            }}
-          />
-        </Dropdown.Menu>
-      </Dropdown>
+  const majorOptions = [
+    { text: "Computer Science" },
+    { text: "Agriculture Studies" },
+    { text: "Forestry Management" },
+    { text: "Biomedical Engineering" }
+  ];
 
-      <Menu.Menu position="right" fluid>
-        <Dropdown item search placeholder={searchTitle}>
-          
+  return (
+    <Menu secondary>
+      <Menu.Item>
+        <Dropdown item text={title}>
+          <Dropdown.Menu>
+            <Dropdown.Item
+              text="Major"
+              onClick={() => {
+                setTitle((title = "Major"));
+              }}
+            />
+            <Dropdown.Item
+              text="Year"
+              onClick={() => {
+                setTitle((title = "Year"));
+              }}
+            />
+            <Dropdown.Item
+              text="Country of Origin"
+              onClick={() => {
+                setTitle((title = "Country of Origin"));
+              }}
+            />
+            <Dropdown.Item
+              text="Sex"
+              onClick={() => {
+                setTitle((title = "Sex"));
+              }}
+            />
+            <Dropdown.Item
+              text="Ethnicity"
+              onClick={() => {
+                setTitle((title = "Ethnicity"));
+              }}
+            />
+            <Dropdown.Item
+              text="Class Sharing"
+              onClick={() => {
+                setTitle((title = "Class Sharing"));
+              }}
+            />
+          </Dropdown.Menu>
         </Dropdown>
+      </Menu.Item>
+
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Dropdown
+            placeholder={searchTitle}
+            item
+            search
+            options={majorOptions}
+          />
+        </Menu.Item>
       </Menu.Menu>
     </Menu>
   );
