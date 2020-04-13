bc5274882a38a9876857314cc6966b19c27d660e
MarianaTorresTorres <mari.torret@gmail.com>
2020-04-13 14:04:19 -0400

    tab panes done, delete database and confirm deletion almost done

--- src/pages/Archives.js
+++ src/pages/Archives.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Container, Grid, Button, Modal } from "semantic-ui-react";
+import {
+  Container,
+  Grid,
+  Button,
+  Modal,
+  Card,
+  Tab,
+  Segment,
+  Input
+} from "semantic-ui-react";
 
 import Title from "../components/Title";
 
@@ -18,42 +27,165 @@ function Archives() {
     }
   };
 
-  return (
-    <>
-      <Title title="Archives" adminPath={window.location.pathname} />
-      <Container className="body">
+  var membershipPane = {
+    menuItem: { content: "Membership", icon: "users" },
+    render: () => (
+      <Tab.Pane>
         <Grid>
           <Grid.Row>
             <Grid.Column>
-              <Button onClick={() => openModal("deleteSHPE")}>
-                Delete the entire website
-              </Button>
+              <Button color="green">Download as a CSV</Button>
             </Grid.Column>
           </Grid.Row>
+          <Grid.Row>
+            <p>*membership status will be here*</p>
+          </Grid.Row>
         </Grid>
-      </Container>
+      </Tab.Pane>
+    )
+  };
+
+  var listServPane = {
+    menuItem: { content: "List Serv", icon: "address book outline" },
+    render: () => (
+      <Tab.Pane>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Button color="green">Download as a CSV</Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <p>*List serv will be here*</p>
+          </Grid.Row>
+        </Grid>
+      </Tab.Pane>
+    )
+  };
+
+  var graduatingPane = {
+    menuItem: { content: "Graduating", icon: "graduation" },
+    render: () => (
+      <Tab.Pane>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Button color="green">Download as a CSV</Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <h3>Cesar and Eddy, I guess</h3>
+          </Grid.Row>
+        </Grid>
+      </Tab.Pane>
+    )
+  };
+
+  var alumniPane = {
+    menuItem: { content: "Alumni", icon: "suitcase" },
+    render: () => (
+      <Tab.Pane>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Button color="green">Download as a CSV</Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <h3>*Alumni stuff will be here*</h3>
+          </Grid.Row>
+        </Grid>
+      </Tab.Pane>
+    )
+  };
+
+  var dangerPane = {
+    menuItem: { content: "Danger Zone", icon: "warning sign" },
+    render: () => (
+      <Tab.Pane>
+        <Grid>
+          <Grid.Row>
+            <h2>Delete All of the Databases</h2>
+          </Grid.Row>
+          <Grid.Row>
+            <p>
+              By pressing the Wipe Databases button, you will permanently delete
+              all the information in this website. Once deleted, there is no
+              coming back. Please be certain of the consequences.
+            </p>
+          </Grid.Row>
+          <Grid.Row>
+            <Button color="red" onClick={() => openModal("deleteSHPE")}>
+              Wipe Databases
+            </Button>
+          </Grid.Row>
+        </Grid>
+      </Tab.Pane>
+    )
+  };
+
+  return (
+    <div className="body">
+      <Title title="Archives" adminPath={window.location.pathname} />
+      <Segment basic>
+        <Container>
+          <Tab
+            panes={[
+              membershipPane,
+              listServPane,
+              graduatingPane,
+              alumniPane,
+              dangerPane
+            ]}
+          />
+        </Container>
+      </Segment>
 
       <Modal open={deleteSHPEModal} size="tiny">
         <Modal.Header>
-          <h2>SHPE is now deleted</h2>
+          <h2>Confirm Deletion</h2>
         </Modal.Header>
         <Modal.Content>
           <Grid>
             <Grid.Row>
               <Grid.Column>
+                <p>This action cannot be undone, are you sure?</p>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <p>
+                  Type "I am sure that I want to delete all databases" below to
+                  confirm
+                </p>
+                <Input focus placeholder="Type here..." />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={2}>
+              <Grid.Column>
+                <Button
+                  type="reset"
+                  color="red"
+                  onClick={() => closeModal("deleteSHPE")}
+                >
+                  Delete
+                </Button>
+              </Grid.Column>
+              <Grid.Column>
                 <Button
                   type="reset"
                   color="grey"
+                  floated="right"
                   onClick={() => closeModal("deleteSHPE")}
                 >
-                  Okay
+                  Cancel
                 </Button>
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Modal.Content>
       </Modal>
-    </>
+    </div>
   );
 }
 
