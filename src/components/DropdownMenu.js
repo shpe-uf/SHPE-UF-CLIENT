f88d5eb1fbb719e12084e6a7440ee325df8f5f7a
despantoso@ufl.edu <despantoso@ufl.edu>
2020-05-17 15:13:27 -0400

    All of David's work. Transferring to Juan

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -6,100 +6,96 @@ import {
   Container,
   Label,
   Icon,
-  Grid
+  Grid,
+  Form,
 } from "semantic-ui-react";
 
 function DropdownMenu() {
   var [title, setTitle] = useState("Major");
   var [options, setOptions] = useState([
- "Computer Science", "Computer Engineering", "Digital Arts and Sciences", "Biomedical Engineering"
+    "Computer Science",
+    "Computer Engineering",
+    "Digital Arts and Sciences",
+    "Biomedical Engineering",
   ]);
   var searchTitle = "Search " + title;
   /*
   <Search.Category
   text={nameOfOption.text}
   onClick={() => console.log(nameOfOption.text)}
-></Search.Category>
-*/
+  ></Search.Category>
+  */
 
   return (
-    <Container>
-      <Menu borderless stackable>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column stretched width={14}>
-              <Menu.Item>
-                <Search fluid placeholder={searchTitle} results={options} onClick={()=> console.log(options)}/>
-              </Menu.Item>
-            </Grid.Column>
-            <Grid.Column width={2}>
-              <Menu.Item position="right">
-                <Dropdown className="background-dropdown" item text={title}>
-                  <Dropdown.Menu>
-                    <Dropdown.Item
-                      text="Major"
-                      onClick={() => {
-                        setTitle((title = "Major"));
-                        setOptions([
-                          { text: "Computer Science" },
-                          { text: "Computer Engineering" },
-                          { text: "Digital Arts and Sciences" },
-                          { text: "Biomedical Engineering" }
-                        ]);
-                      }}
-                    />
-                    <Dropdown.Item
-                      text="Year"
-                      onClick={() => {
-                        setTitle((title = "Year"));
-                        setOptions([
-                          { text: "1st Year" },
-                          { text: "2nd Year" }
-                        ]);
-                      }}
-                    />
-                    <Dropdown.Item
-                      text="Country of Origin"
-                      onClick={() => {
-                        setTitle((title = "Country of Origin"));
-                        setOptions([{ text: "Cuba" }, { text: "Venezuela" }]);
-                      }}
-                    />
-                    <Dropdown.Item
-                      text="Sex"
-                      onClick={() => {
-                        setTitle((title = "Sex"));
-                        setOptions([{ text: "Female" }, { text: "Male" }]);
-                      }}
-                    />
-                    <Dropdown.Item
-                      text="Ethnicity"
-                      onClick={() => {
-                        setTitle((title = "Ethnicity"));
-                        setOptions([{ text: "Hispanic/Latino" }]);
-                      }}
-                    />
-                    <Dropdown.Item
-                      text="Class Sharing"
-                      onClick={() => {
-                        setTitle((title = "Class Sharing"));
-                        setOptions([{ text: "COP4600" }]);
-                      }}
-                    />
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Menu.Item>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+    <>
+      <Menu stackable>
+        <Menu.Item>
+          <Form onSubmit={() => console.log("HELLO")}>
+            <Form.Field>
+              <input placeholder={searchTitle} />
+            </Form.Field>
+          </Form>
+        </Menu.Item>
+        <Menu.Item position="right">
+          <Dropdown className="background-dropdown" item text={title}>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                text="Major"
+                onClick={() => {
+                  setTitle((title = "Major"));
+                  setOptions([
+                    { text: "Computer Science" },
+                    { text: "Computer Engineering" },
+                    { text: "Digital Arts and Sciences" },
+                    { text: "Biomedical Engineering" },
+                  ]);
+                }}
+              />
+              <Dropdown.Item
+                text="Year"
+                onClick={() => {
+                  setTitle((title = "Year"));
+                  setOptions([{ text: "1st Year" }, { text: "2nd Year" }]);
+                }}
+              />
+              <Dropdown.Item
+                text="Country of Origin"
+                onClick={() => {
+                  setTitle((title = "Country of Origin"));
+                  setOptions([{ text: "Cuba" }, { text: "Venezuela" }]);
+                }}
+              />
+              <Dropdown.Item
+                text="Sex"
+                onClick={() => {
+                  setTitle((title = "Sex"));
+                  setOptions([{ text: "Female" }, { text: "Male" }]);
+                }}
+              />
+              <Dropdown.Item
+                text="Ethnicity"
+                onClick={() => {
+                  setTitle((title = "Ethnicity"));
+                  setOptions([{ text: "Hispanic/Latino" }]);
+                }}
+              />
+              <Dropdown.Item
+                text="Class Sharing"
+                onClick={() => {
+                  setTitle((title = "Class Sharing"));
+                  setOptions([{ text: "COP4600" }]);
+                }}
+              />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
       </Menu>
-
-      <p></p>
+      <br></br>
       <Label circular>
         Computer Science
         <Icon name="delete" />
       </Label>
-    </Container>
+    </>
   );
 }
 
