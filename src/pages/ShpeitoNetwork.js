843cdb9a0315ef8548891dc19b0cc04be349a575
despantoso@ufl.edu <despantoso@ufl.edu>
2020-08-24 15:54:16 -0400

    feat(SHPEito NoClass Modal): Simple modal opening up if no classes

--- src/pages/ShpeitoNetwork.js
+++ src/pages/ShpeitoNetwork.js
@@ -10,6 +10,7 @@ import {
   Modal,
   List,
 } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { FETCH_USERS_QUERY } from "../util/graphql";
 import gql from "graphql-tag";
@@ -31,31 +32,51 @@ function ShpeitoNetwork(props) {
     })
   );
 
+  const [open, setOpen] = React.useState(true);
+
   let { data, loading } = useQuery(FETCH_USERS_QUERY);
   let users = [];
-  
+
   let {
-    user: { id }
+    user: { id },
   } = useContext(AuthContext);
 
   let user = useQuery(FETCH_USER_QUERY, {
     variables: {
-      userId: id
-    }
+      userId: id,
+    },
   }).data.getUser;
 
-  if(!loading && data) {
+  if (!loading && data) {
     users = data.getUsers.filter(function (user) {
       let fullName = user.firstName.concat(" ").concat(user.lastName);
-      return ( 
-        (filter.major.length      === 0 ? true : filter.major.includes(user.major))           &&
-        (filter.year.length       === 0 ? true : filter.year.includes(user.year))             &&
-        (filter.graduating.length === 0 ? true : filter.graduating.includes(user.graduating)) &&
-        (filter.country.length    === 0 ? true : filter.country.includes(user.country))       &&
-        (filter.classes.length    === 0 ? true : filter.classes.includes(user.classes))       &&
-        (filter.name.length       === 0 ? true : filter.name.map(n => user.firstName.toLowerCase().includes(n.toLowerCase())).includes(true) ||
-                                                 filter.name.map(n => user.lastName.toLowerCase().includes(n.toLowerCase() )).includes(true) ||
-                                                 filter.name.map(n => fullName.toLowerCase().includes(n.toLowerCase())).includes(true))
+      return (
+        (filter.major.length === 0
+          ? true
+          : filter.major.includes(user.major)) &&
+        (filter.year.length === 0 ? true : filter.year.includes(user.year)) &&
+        (filter.graduating.length === 0
+          ? true
+          : filter.graduating.includes(user.graduating)) &&
+        (filter.country.length === 0
+          ? true
+          : filter.country.includes(user.country)) &&
+        (filter.classes.length === 0
+          ? true
+          : filter.classes.includes(user.classes)) &&
+        (filter.name.length === 0
+          ? true
+          : filter.name
+              .map((n) =>
+                user.firstName.toLowerCase().includes(n.toLowerCase())
+              )
+              .includes(true) ||
+            filter.name
+              .map((n) => user.lastName.toLowerCase().includes(n.toLowerCase()))
+              .includes(true) ||
+            filter.name
+              .map((n) => fullName.toLowerCase().includes(n.toLowerCase()))
+              .includes(true))
       );
     });
   }
@@ -73,43 +94,68 @@ function ShpeitoNetwork(props) {
           <Segment disabled loading>
             <div style={{ height: "400px" }} />
           </Segment>
-        ) : users.length > 0 ? (
-          <>
-            <p></p>
-            {console.log(user)}
-            <Segment>
-              <Card.Group stackable itemsPerRow="4">
-                {users.map((shpeito) => (
-                  <Card>
-                    <Image src={placeholder} className="card-image" />
-                    <Card.Content>
-                      <Card.Header>
-                        {shpeito.firstName} {shpeito.lastName}
-                      </Card.Header>
-                      <Card.Meta>{shpeito.major}</Card.Meta>
-                      <Card.Meta>{shpeito.year}</Card.Meta>
-                      <Card.Description>
-                        <Icon name="world"></Icon>
-                        {shpeito.country}
-                      </Card.Description>
-                      <p></p>
-                      <Modal
-                        trigger={
-                          <Button
-                            fluid
-                            content="View Classes"
-                            icon="book"
-                            labelPosition="left"
-                          />
-                        }
-                        size="mini"
-                        closeIcon
-                      >
-                        <Modal.Header>
-                          {shpeito.firstName}'s Classes
-                        </Modal.Header>
-                        <Modal.Content>
-                          <p></p>
+        ) : user ? (
+          user.classes.length == 0 ? (
+            <Modal
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+              open={open}
+            >
+              <Modal.Header>
+                <h3>Hello {user.firstName},</h3>
+              </Modal.Header>
+              <Modal.Content>
+                <Modal.Description>
+                  <p>&emsp;It seems like you currently have no classes.</p>
+                  <p>
+                    &emsp;Please click on "Continue" to register your classes.
+                  </p>
+                </Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button color="red" onClick={() => setOpen(false)}>
+                  CANCEL
+                </Button>
+                <Button color="green">CONTINUE</Button>
+              </Modal.Actions>
+            </Modal>
+          ) : users.length > 0 ? (
+            <>
+              {console.log(user.classes.length)}
+              <p></p>
+              <Segment>
+                <Card.Group stackable itemsPerRow="4">
+                  {users.map((shpeito) => (
+                    <Card>
+                      <Image src={placeholder} className="card-image" />
+                      <Card.Content>
+                        <Card.Header>
+                          {shpeito.firstName} {shpeito.lastName}
+                        </Card.Header>
+                        <Card.Meta>{shpeito.major}</Card.Meta>
+                        <Card.Meta>{shpeito.year}</Card.Meta>
+                        <Card.Description>
+                          <Icon name="world"></Icon>
+                          {shpeito.country}
+                        </Card.Description>
+                        <p></p>
+                        <Modal
+                          trigger={
+                            <Button
+                              fluid
+                              content="View Classes"
+                              icon="book"
+                              labelPosition="left"
+                            />
+                          }
+                          size="mini"
+                          closeIcon
+                        >
+                          <Modal.Header>
+                            {shpeito.firstName}'s Classes
+                          </Modal.Header>
+                          <Modal.Content>
+                            <p></p>
                             {shpeito.classes.length > 0 ? (
                               shpeito.classes.map((className) => (
                                 <List divided className="shpeito-spacing">
@@ -129,14 +175,17 @@ function ShpeitoNetwork(props) {
                               </div>
                             )}
                             <p></p>
-                        </Modal.Content>
-                      </Modal>
-                    </Card.Content>
-                  </Card>
-                ))}
-              </Card.Group>
-            </Segment>
-          </>
+                          </Modal.Content>
+                        </Modal>
+                      </Card.Content>
+                    </Card>
+                  ))}
+                </Card.Group>
+              </Segment>
+            </>
+          ) : (
+            console.log("USER IS UNDEFINED")
+          )
         ) : (
           <div style={{ paddingBottom: 16 }}>
             <p></p>
