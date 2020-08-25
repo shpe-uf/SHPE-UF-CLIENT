c78006515af5f049c139563961f52b623ce93e4a
juansuh <juansuhr@gmail.com>
2020-08-24 21:47:14 -0400

    fix profile form internships and links

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -379,7 +379,7 @@ function Profile() {
                     onKeyPress={(e)=> (e.key === 'Enter') && addToArray(e,'internship')}
                     onChange={(e) => setInternship(e.target.value)}
                     action={{
-                      onClick: (e)=>{addToArray(e,'class')},
+                      onClick: (e)=>{addToArray(e,'internship')},
                       icon: 'plus'
                     }}
                   />
@@ -406,7 +406,7 @@ function Profile() {
                     onKeyPress={(e)=> (e.key === 'Enter') && addToArray(e,'socialMedia')}
                     onChange={(e) => setSocialMedia(e.target.value)}
                     action={{
-                      onClick: (e)=>{addToArray(e,'class')},
+                      onClick: (e)=>{addToArray(e,'socialMedia')},
                       icon: 'plus'
                     }}
                   />
