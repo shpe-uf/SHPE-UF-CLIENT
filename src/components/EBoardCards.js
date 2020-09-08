23ca972d9573763c1126a75a2eef95a56e0579dc
juansuh <juansuhr@gmail.com>
2020-09-08 05:59:42 -0400

    eboard

--- src/components/EBoardCards.js
+++ src/components/EBoardCards.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Card, Icon } from "semantic-ui-react";
 
-import president from "../assets/images/eboard/president.png";
-import graduate from "../assets/images/eboard/graduate.jpg";
+import president from "../assets/images/eboard/president.jpeg";
+import graduate from "../assets/images/eboard/graduate.jpeg";
 import secretary from "../assets/images/eboard/secretary.jpg";
-import treasurer from "../assets/images/eboard/treasurer.jpg";
-import marketing from "../assets/images/eboard/marketing.jpg";
-import corporate from "../assets/images/eboard/corporate.jpg";
-import technology from "../assets/images/eboard/technology.jpg";
-import external from "../assets/images/eboard/external.png";
-import internal from "../assets/images/eboard/internal.png";
+import treasurer from "../assets/images/eboard/treasurer.jpeg";
+import marketing from "../assets/images/eboard/marketing.jpeg";
+import corporate from "../assets/images/eboard/corporate.jpeg";
+import technology from "../assets/images/eboard/technology.jpeg";
+import external from "../assets/images/eboard/external.jpeg";
+import internal from "../assets/images/eboard/internal.jpeg";
 
 function email(email) {
   return (
@@ -26,63 +26,63 @@ function EBoardCards() {
       <Card
         fluid
         image={president}
-        header="Jose Luis Alegria"
+        header="Federico Roye"
         meta="President"
         extra={email("president.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={graduate}
-        header="Nicholas Abuid"
+        header="Baltazar Lopez Sardi"
         meta="Graduate Coordinator"
         extra={email("graduate.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={secretary}
-        header="Isabella Campbell"
+        header="Kelly Salas Diaz"
         meta="Secretary"
         extra={email("secretary.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={treasurer}
-        header="Anthony Moreno"
+        header="Tomas Cusi"
         meta="Treasurer"
         extra={email("treasurer.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={marketing}
-        header="Jonathan Morales"
+        header="Melody Morales Roja"
         meta="VP of Marketing"
         extra={email("marketing.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={corporate}
-        header="Jonathan Medina"
+        header="Ariana Ortega"
         meta="VP of Corporate Affairs"
         extra={email("corporate.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={technology}
-        header="Rodrigo Lobo"
+        header="Gabriel Rodríguez Torres"
         meta="VP of Technology"
         extra={email("vptech.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={external}
-        header="Domingo Alegria"
+        header="Lisa Duran"
         meta="VP of External Affairs"
         extra={email("vpexternal.shpeuf@gmail.com")}
       />
       <Card
         fluid
         image={internal}
-        header="Duncan Ross"
+        header="Caterina Zientek"
         meta="VP of Internal Affairs"
         extra={email("vpinternal.shpeuf@gmail.com")}
       />
