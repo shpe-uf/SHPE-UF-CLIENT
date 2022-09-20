d890d23d6ca4bb1f70396230b314f70f73ea920a
Yair Temkin <yairtemkin@gmail.com>
2022-09-20 15:00:22 -0400

    frontend fixes

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -11,10 +11,10 @@ const httpLink = createHttpLink({
   uri: "https://shpe-uf.herokuapp.com/",
 });
 
-//for development, comment this out in your PR
-// const httpLink = createHttpLink({
-//   uri: "http://localhost:5000/",
-// });
+/*for development, comment this out in your PR
+const httpLink = createHttpLink({
+   uri: "http://localhost:5000/",
+});*/
 
 const authLink = setContext(() => {
   const token = localStorage.getItem("jwtToken");
