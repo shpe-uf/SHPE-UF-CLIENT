3d9f06538bd5d95ffaa16728afac565910aa067a
yairtemkin <72417965+yairtemkin@users.noreply.github.com>
2022-09-20 14:48:22 -0400

    Revert "removing potential cors fixes that didnt work 🙃"

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -21,9 +21,6 @@ const authLink = setContext(() => {
   return {
     headers: {
       Authorization: token ? `Bearer ${token}` : "",
-      /*"Access-Control-Allow-Origin": "*",
-      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-      'Access-Control-Allow-Origin': 'https://shpe-uf.herokuapp.com/'*/
     },
   };
 });
