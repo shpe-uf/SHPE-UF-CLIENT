91f5ec4696447607d1fc308c6ac3bbc25934c092
w-omar <wocollado@gmail.com>
2023-01-28 19:56:24 -0500

    everyones got a picture

--- src/components/DevTeamCards.js
+++ src/components/DevTeamCards.js
@@ -10,24 +10,24 @@ import andrea from "../assets/images/team/andrea.jpg";
 import mateo from "../assets/images/team/mateo.jpg";
 import lucas from "../assets/images/team/lucas.jpeg";
 import isabella from "../assets/images/team/isabella.jpg";
-//import joel from "../assets/images/team/joel.jpg";
+import joel from "../assets/images/team/joel.jpeg";
 import jesus from "../assets/images/team/jesus.jpg";
 import temp from "../assets/images/team/placeholder.png";
 
 function DevTeamCards() {
   return (
     <>
-      <Card.Group itemsPerRow={4}>
+      <Card.Group itemsPerRow={3}>
         <Card fluid image={yair} header="Yair Temkin" meta="Scrum Master" />
         <Card fluid image={omar} header="Omar Collado" meta="Project Manager" />
-        <Card fluid image={fer} header="Fernando Rauseo" meta="Project Manager" />
+        
         <Card fluid image={ju} header="Julia Chancey" meta="Senior Developer" />
         <Card fluid image={victoria} header="Victoria De Alba" meta="Senior Developer" />
         <Card fluid image={mateo} header="Mateo Slivka" meta="Senior Developer" />
         <Card fluid image={lucas} header="Lucas Skaf" meta="Junior Developer" />
         <Card fluid image={isabella} header="Isabella Roman" meta="Junior Developer" />
         <Card fluid image={jesus} header="Jesus Jurado" meta="Junior Developer" />
-        <Card fluid image={temp} header="Joel Aloma" meta="Junior Developer" />
+        <Card fluid image={joel} header="Joel Aloma" meta="Junior Developer" />
       </Card.Group>
     </>
   );
