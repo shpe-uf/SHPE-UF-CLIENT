84f64a0e65dfb07dbd9f8f7d27fbf6818f7bd8ae
yairtemkin <72417965+yairtemkin@users.noreply.github.com>
2023-02-03 14:15:50 -0500

    Updates to dev team, points page and SHPE rentals (#138)
    * Percentile to Percentage change
    
    * Added changes based on reviews
    
    * uncommenting dropdown (#130)
    
    * Fixed dimensions (#134)
    
    Co-authored-by: Isabella Roman <Isabella Roman>
    
    * Updating Dev team (#136)
    
    * 4 new members
    
    * everyone has a photo except Joel
    
    * everyones got a picture
    
    ---------
    
    Co-authored-by: Fernando Rauseo <FRL200>
    
    * Bump json5 from 1.0.1 to 1.0.2 (#131)
    
    Bumps [json5](https://github.com/json5/json5) from 1.0.1 to 1.0.2.
    - [Release notes](https://github.com/json5/json5/releases)
    - [Changelog](https://github.com/json5/json5/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/json5/json5/compare/v1.0.1...v1.0.2)
    
    ---
    updated-dependencies:
    - dependency-name: json5
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump express from 4.17.1 to 4.18.2 (#129)
    
    Bumps [express](https://github.com/expressjs/express) from 4.17.1 to 4.18.2.
    - [Release notes](https://github.com/expressjs/express/releases)
    - [Changelog](https://github.com/expressjs/express/blob/master/History.md)
    - [Commits](https://github.com/expressjs/express/compare/4.17.1...4.18.2)
    
    ---
    updated-dependencies:
    - dependency-name: express
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump qs from 6.5.2 to 6.5.3 (#128)
    
    Bumps [qs](https://github.com/ljharb/qs) from 6.5.2 to 6.5.3.
    - [Release notes](https://github.com/ljharb/qs/releases)
    - [Changelog](https://github.com/ljharb/qs/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/ljharb/qs/compare/v6.5.2...v6.5.3)
    
    ---
    updated-dependencies:
    - dependency-name: qs
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump decode-uri-component from 0.2.0 to 0.2.2 (#127)
    
    Bumps [decode-uri-component](https://github.com/SamVerschueren/decode-uri-component) from 0.2.0 to 0.2.2.
    - [Release notes](https://github.com/SamVerschueren/decode-uri-component/releases)
    - [Commits](https://github.com/SamVerschueren/decode-uri-component/compare/v0.2.0...v0.2.2)
    
    ---
    updated-dependencies:
    - dependency-name: decode-uri-component
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Isabella Roman <Isabella Roman>
    Co-authored-by: Isabella Roman <115611774+IsRo06@users.noreply.github.com>
    Co-authored-by: w-omar <39841679+w-omar@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/components/DevTeamCards.js
+++ src/components/DevTeamCards.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "semantic-ui-react";
+import { Card, CardHeader } from "semantic-ui-react";
 
 import yair from "../assets/images/team/yair.jpg";
 import fer from "../assets/images/team/fernando.jpg";
@@ -8,18 +8,27 @@ import omar from "../assets/images/team/omar.jpg";
 import victoria from "../assets/images/team/victoria.jpg";
 import andrea from "../assets/images/team/andrea.jpg";
 import mateo from "../assets/images/team/mateo.jpg";
-//import temp from "../assets/images/team/placeholder.png";
+import lucas from "../assets/images/team/lucas.jpeg";
+import isabella from "../assets/images/team/isabella.jpg";
+import joel from "../assets/images/team/joel.jpeg";
+import jesus from "../assets/images/team/jesus.jpg";
+import temp from "../assets/images/team/placeholder.png";
 
 function DevTeamCards() {
   return (
     <>
-      <Card fluid image={yair} header="Yair Temkin" meta="Scrum Master" />
-      <Card fluid image={fer} header="Fernando Rauseo" meta="Project Manager" />
-      <Card fluid image={ju} header="Julia Chancey" meta="Senior Developer" />
-      <Card fluid image={omar} header="Omar Collado" meta="Senior Developer" />
-      <Card fluid image={victoria} header="Victoria De Alba" meta="Senior Developer" />
-      <Card fluid image={andrea} header="Andrea Moreno" meta="Senior Developer" />
-      <Card fluid image={mateo} header="Mateo Slivka" meta="Senior Developer" />
+      <Card.Group itemsPerRow={3}>
+        <Card fluid image={yair} header="Yair Temkin" meta="Scrum Master" />
+        <Card fluid image={omar} header="Omar Collado" meta="Project Manager" />
+        
+        <Card fluid image={ju} header="Julia Chancey" meta="Senior Developer" />
+        <Card fluid image={victoria} header="Victoria De Alba" meta="Senior Developer" />
+        <Card fluid image={mateo} header="Mateo Slivka" meta="Senior Developer" />
+        <Card fluid image={lucas} header="Lucas Skaf" meta="Junior Developer" />
+        <Card fluid image={isabella} header="Isabella Roman" meta="Junior Developer" />
+        <Card fluid image={jesus} header="Jesus Jurado" meta="Junior Developer" />
+        <Card fluid image={joel} header="Joel Aloma" meta="Junior Developer" />
+      </Card.Group>
     </>
   );
 }
