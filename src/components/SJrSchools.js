7a218b22ab81943a195c4e80e8eb4769895f15d7
victoriadealba <victoriadealba@ufl.edu>
2023-03-27 19:57:03 -0400

    added heading to partner schools

--- src/components/SJrSchools.js
+++ src/components/SJrSchools.js
@@ -3,6 +3,7 @@ import React from 'react'
 const SJrSchools = () =>{
   return(
     <div className='SJrSchools'>
+      <h2> Partner Schools</h2>
       <p> Stuff about Partner Schools will go here </p>
     </div>
 
