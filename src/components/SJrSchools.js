9b903188819a51108ab40c412b94efd529602f36
victoriadealba <victoriadealba@ufl.edu>
2023-04-04 11:54:53 -0400

    Need to actual make functional cards

--- src/components/SJrSchools.js
+++ src/components/SJrSchools.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import {Card, Image } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+const src = '/images/wireframe/white-image.png'
 
 const SJrSchools = () =>{
   return(
     <div className='SJrSchools'>
       <h2> Partner Schools</h2>
-      <p> Stuff about Partner Schools will go here </p>
+      <Card.Group itemsPerRow={3}>
+      <Card color='red' image={src} />
+    <Card color='orange' image={src} />
+    <Card color='yellow' image={src} />
+    <Card color='olive' image={src} />
+    <Card color='green' image={src} />
+    <Card color='teal' image={src} />
+    <Card color='blue' image={src} />
+    <Card color='violet' image={src} />
+    <Card color='purple' image={src} />
+    <Card color='pink' image={src} />
+    <Card color='brown' image={src} />
+    <Card color='grey' image={src} />
+      </Card.Group>
     </div>
 
   );
