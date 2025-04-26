9cfb4ec3839e92f44354e4819c99de7e99bd721a
mxqrz <danielpermanez@gmail.com>
2025-04-26 18:29:55 -0400

    initial connit

--- src/pages/public/About.js
+++ src/pages/public/About.js
@@ -1,124 +1,126 @@
-import React from "react";
-import { Container, Grid, Image } from "semantic-ui-react";
+import React from 'react';
 
-import {about1, about2, about3} from "../../assets/images/about"
+import polygonImage from '../../assets/images/Polygon-image.png';
+import introGears from '../../assets/images/intro-panel-gears.svg';
+import wavyMaskGroup from '../../assets/images/Wavy Mask group.svg';
+import image6 from '../../assets/images/image 6.png';
+import rectangle124 from '../../assets/images/Rectangle 124.png';
+import rectangle125 from '../../assets/images/Rectangle 125.png';
+import rectangle126 from '../../assets/images/Rectangle 126.png';
 
-function About() {
+const About = () => {
+  const numberOfHexagons = 17;
+  const hexagonItems = Array.from({ length: numberOfHexagons });
+
   return (
-    <div className="body">
-      <div className="masthead masthead-about">
-        <div className="overlay-blue">
-          <Container>
-            <h1 className="masthead-title text-white">About Us</h1>
-          </Container>
+    <div className="about-us-container">
+      <div className="intro-section">
+        <div className="main">
+          <div className="hex-container">
+            {hexagonItems.map((_, index) => (
+              <div
+                key={`hex-${index}`}
+                className="hex"
+                style={{ backgroundImage: `url(${polygonImage})` }}
+              ></div>
+            ))}
+          </div>
+        </div>
+        <div className="intro-text">
+          <div className="intro-section-header">
+            <h1 className="intro-header">Empowering Hispanic <br /> Students to Thrive in <br /> <span style={{ color: '#FD652F' }}>STEM</span></h1>
+          </div>
+          <div className="intro-blurb">
+            <p>SHPE UF supports and empowers <br /> Hispanic students in STEM
+              <br /> through resources, mentorship, <br /> and opportunities to reach their <br /> full potential.</p>
+          </div>
         </div>
       </div>
 
-      <Container>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column>
-              <h2>History</h2>
-              <p>
-                The Society of Hispanic Professional Engineers (SHPE) was
-                founded in Los Angeles, California, in 1974 by a group of
-                engineers employed by the city of Los Angeles. Their objective
-                was to form a national organization of professional engineers to
-                serve as role models in the Hispanic community.
-              </p>
-              <p>
-                The concept of networking was the key basis for the
-                organization. SHPE quickly established two student chapters to
-                begin the network that would grow to encompass the nation as
-                well as reach countries outside the United States. Today, SHPE
-                enjoys a strong but independent network of professional and
-                student chapters throughout the nation.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-        <Grid>
-          <Grid.Row>
-            <Grid.Column>
-              <h2>UF Chapter</h2>
-              <p>
-                The Society of Hispanic Professional Engineers Chapter at the
-                University of Florida (SHPE UF) was formerly known as the
-                Hispanic Engineering Society. It was founded in the fall of 1982
-                in an effort to provide Hispanic engineers, mathematicians, and
-                scientists with opportunities to develop as professionals while
-                offering an amiable social environment.
-              </p>
-              <p>
-                Since its inception it has prompted the recruitment, retention,
-                and graduation of Hispanic students in the fields of science,
-                technology, engineering, and mathematics. The society
-                coordinates community outreach, corporate exposition meetings,
-                academic development programs, and non-technical events. This is
-                to ensure SHPE UF members are presented with the opportunities
-                necessary to succeed professionally, academically, and socially
-                as Hispanic professionals.
-              </p>
-              <p>
-                The University of Florida's Hispanic Engineering Society
-                established a strong connection with the Society of Hispanic
-                Professional Engineers, but it was not until the summer of 2001
-                that the society underwent a name change in order to affirm its
-                close relationship with SHPE National. Today, The University of
-                Florida Chapter holds a strong and enriching relationship with
-                SHPE National, which has helped increase membership in the
-                society.
-              </p>
-              <p>
-                In previous years, SHPE UF has experienced exponential growth by
-                employing strategic ways to improve previous recruitment and
-                retention initiatives through execution of events such as the
-                Goals for Tomorrow scholarship and soccer tournament, numerous socials,
-                SHPE Hackathon, and many more.
-              </p>
-              <p>
-                SHPE UF hosted the most successful BBQ with Industry yet, with over 20 companies
-                and over 200 students attending the event. The chapter also held a ShadowSHPE day
-                to bring together its 3 SHPE Jr. chapters across the state to experience what it
-                is to be a Gator Engineer. Additionally, the chapter is in offers mentorship programs
-                with companies such as Boeing and Honeywell.
-              </p>
-              <p>
-                The dedication and commitment shown by past and present members led to an increase
-                in active participation of the chapter at a local, regional, and national level.
-                This drive to further involvement in the society led to a strong presence from the
-                chapter at National Conference and other events. With over 280 members attending,
-                The University of Florida had the largest number of members present at this year's conference.
-                The determination of the chapter's leaders has also led to incredible involvement and retention
-                of first-year students in the society and has made the chapter grow to an astonishing 480 members
-                in the 2019-2020 year.
-              </p>
-              <p>
-                As a family, we take pride in the passion that is evident among
-                our members. We have observed the success created by past
-                leaders in our chapter, and are honored to have the opportunity
-                to further their excellence.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+      {/*  <img id="intro-gears" src={introGears} alt="Decorative gears graphic" /> */}
 
-      <Grid stackable>
-        <Grid.Row centered columns={3}>
-          <Grid.Column>
-            <Image bordered src={about1} />
-          </Grid.Column>
-          <Grid.Column>
-            <Image bordered src={about2} />
-          </Grid.Column>
-          <Grid.Column>
-            <Image bordered src={about3} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <div className="mission-section">
+        <div className="mission-header-container">
+          <h1><i>OUR MISSION</i></h1>
+        </div>
+        <div className="mission-wrapper">
+          <div className="mission-image-layer">
+            <img className="h-image" id="img-5" src={wavyMaskGroup} alt="Abstract wavy graphic" />
+            <img className="h-image" id="img-4" src={image6} alt="Group of students or professionals" />
+          </div>
+          <div className="mission-container">
+            <p className="desc">
+              At SHPE, our mission is to empower the Hispanic community by providing
+              the tools and support needed to succeed in STEM. We foster growth, connection,
+              and leadership that help our members make an impact in their careers and the world.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="history-section">
+        <div className="header-container">
+          <div style={{ position: 'relative' }}>
+            <h1><i>OUR HISTORY</i></h1>
+          </div>
+        </div>
+        <div className="about-section">
+          <div className="about-container">
+            <p className="desc">
+              Founded in 1974 by a group of engineers in Los Angeles, SHPE aimed to create a national
+              network of Hispanic engineers who could serve as role models and mentors for the community.
+              <br /><br />
+              What started with two student chapters has grown into a nationwide network, with chapters
+              across the U.S. and internationally, including our own here at UF. SHPE continues to empower future
+              generations of Hispanic leaders in STEM.
+            </p>
+          </div>
+          <div className="image-container">
+            <img className="h-image" id="img-1" src={rectangle124} alt="Historical SHPE event or members 1" />
+            <img className="h-image" id="img-2" src={rectangle125} alt="Historical SHPE event or members 2" />
+            <img className="h-image" id="img-3" src={rectangle126} alt="Historical SHPE event or members 3" />
+          </div>
+        </div>
+      </div>
+
+      <div className="vision-container">
+        <div className="vision-header-container">
+          <h1><i>OUR VISION</i></h1>
+        </div>
+        <div className="centering-vision">
+          <div className="vision-section-blurb">
+            <p className="desc">
+              At SHPE UF, we strive to cultivate the next generation of Hispanic
+              STEM professionals. Our vision is to create an inclusive
+              environment where students can thrive academically,
+              professionally, and personally. By offering leadership
+              opportunities, mentorship, and community support, we are
+              shaping the future of Hispanic leaders in STEM at the University
+              of Florida and beyond.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="join-section">
+        <div className="join-header-container">
+          <h1><i>JOIN THE FAMILIA</i></h1>
+        </div>
+        <div className="center-join">
+          <div className="join-section-blurb">
+            <p className="desc">
+              At SHPE UF, we’re more than just a student
+              organization—we’re a family. By joining, you'll gain
+              access to mentorship, leadership development,
+              networking, and the resources needed to succeed in
+              STEM. We're here to support your growth and guide
+              you every step of the way.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default About;
