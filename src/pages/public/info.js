07b458f23643b49e8f3e10d67920a1055870b6e8
Hector Borjas <borjashectora@gmail.com>
2025-08-13 23:01:42 -0400

    info start

---
+++ src/pages/public/info.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Container, Grid, Loader, Label } from "semantic-ui-react";
+import { useQuery } from "@apollo/client";
+
+import Title from "../components/Title";
+import AlumniTable from "../components/AlumniTable";
+import AlumniFilterSelection from "../components/AlumniFilterSelection";
+import { FETCH_ALUMNIS_QUERY } from "../util/graphql";
+
+export default function AlumniDirectory() {
+  
+
+  return (
+    "lool"
+  );
+}
