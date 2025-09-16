82d041b4b56ed58a90bc9b01cebe668a22d71a4f
Hector Borjas <105574084+HectorB06@users.noreply.github.com>
2025-09-16 09:34:41 -0400

    permissions corp. db

--- src/util/AdminRoute.js
+++ src/util/AdminRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Navigate } from "react-router-dom";
 
 function AdminRoute({permission, security, children }) {
-  const authorized = permission.includes('super') || permission.includes(security);
+  const authorized = permission.includes('super') || permission.includes(security) || permission.includes('corporatedatabase');
   return authorized ? children : <Navigate to ="/"/>;
 }
 
