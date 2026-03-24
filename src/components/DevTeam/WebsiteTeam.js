6fffafdf7adbe0d9086df3e262ac8b37cc1d51d8
Mar1789 <116681148+Mar1789@users.noreply.github.com>
2026-03-23 20:54:08 -0400

    Completed Isa's photo change

--- src/components/DevTeam/WebsiteTeam.js
+++ src/components/DevTeam/WebsiteTeam.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Card, Container } from 'semantic-ui-react';
-import { Media } from "../../Media"; 
-import DevTeamCards from './WebsiteTeamCards';
+import React from "react";
+import { Card, Container } from "semantic-ui-react";
+import { Media } from "../../Media";
+import DevTeamCards from "./WebsiteTeamCards";
 
-const WebsiteTeam = () =>{
-  return(
-    <div className='WebsiteTeam'>
-      <h2 > Website Team</h2>
+const WebsiteTeam = () => {
+  return (
+    <div className="WebsiteTeam">
+      <h2> Website Team</h2>
       <br></br>
-     <Container>
-      <Media greaterThanOrEqual="computer">
-        <Card.Group itemsPerRow={3} centered>
-          <DevTeamCards/>
-        </Card.Group>
-      </Media>
-      <Media lessThan="computer">
-        <Card.Group itemsPerRow={1}>
-          <DevTeamCards/>
-        </Card.Group>
-      </Media>
-     </Container>
+      <Container>
+        <Media greaterThanOrEqual="computer">
+          <Card.Group itemsPerRow={3} centered>
+            <DevTeamCards />
+          </Card.Group>
+        </Media>
+        <Media lessThan="computer">
+          <Card.Group itemsPerRow={1}>
+            <DevTeamCards />
+          </Card.Group>
+        </Media>
+      </Container>
     </div>
   );
 };
