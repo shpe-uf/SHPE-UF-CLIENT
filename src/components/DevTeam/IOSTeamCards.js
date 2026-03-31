f5e888363acb5b2856abca6295a2d7f2d5affd65
Monica Coira <monica.coira@ufl.edu>
2026-03-31 19:44:31 -0400

    update iOS dev team cards

--- src/components/DevTeam/IOSTeamCards.js
+++ src/components/DevTeam/IOSTeamCards.js
@@ -1,30 +1,109 @@
 import React from "react";
 import { Card, CardHeader } from "semantic-ui-react";
+import DevTeam from "./css/DevTeam.css";
 
 const team = {
-  daniel: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/Daniel_parra.jpg",
-  ashley: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/dev.png",
-  david_denis: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/david_denis.jpg",
-  mazin: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/Mazin_saleh.jpg",
-  vivian: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/vivian_rincon.jpg",
-  simar: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/simar_khetpal.jpg",
+  david_denis: "https://shpeuf.s3.us-east-1.amazonaws.com/public/team/iOS_Team/david_denis_2026.JPG",
+  mazin: "https://shpeuf.s3.us-east-1.amazonaws.com/public/team/iOS_Team/mazin_saleh.JPG",
   matthew: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/matthew_segura.jpg",
-  david_vera: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/dev.png",
-
-
-}
+  carlos: "https://shpeuf.s3.us-east-1.amazonaws.com/public/team/iOS_Team/carlos_ch%C3%A1vez.jpg",
+  placeholder: "https://shpeuf.s3.amazonaws.com/public/team/iOS_Team/dev.png",
+};
 
 function DevTeamCards() {
   return (
     <>
-        <Card fluid image={team.daniel} header="Daniel Parra" meta="Swift Lead" />
-        <Card fluid image={team.ashley} header="Ashley Guerra" meta="Project Manager" />    
-        <Card fluid image={team.david_denis} header="David Denis" meta="iOS Developer" />     
-        <Card fluid image={team.mazin} header="Mazin Saleh" meta="iOS Developer" />   
-        <Card fluid image={team.vivian} header="Vivian Rincon" meta="iOS Developer" />   
-        <Card fluid image={team.simar} header="Simar Kheptal" meta="iOS Developer" />   
-        <Card fluid image={team.matthew} header="Matthew Segura" meta="iOS Developer" />   
-        <Card fluid image={team.david_vera} header="David Vera" meta="iOS Developer" /> 
+      <Card
+        fluid
+        image={team.matthew}
+        header="Matthew Segura"
+        meta="Swift Lead"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.carlos}
+        header="Carlos Chávez"
+        meta="Project Manager"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Josh Carron"
+        meta="Junior Project Manager"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.david_denis}
+        header="David Denis"
+        meta="Senior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Gabriella Garcia"
+        meta="Senior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Alex Milanes"
+        meta="Senior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Javier Martinez"
+        meta="Senior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Pablo Pupo"
+        meta="Senior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.mazin}
+        header="Mazin Saleh"
+        meta="Senior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Kaylee Driscoll"
+        meta="Junior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Max Lopez Mateos"
+        meta="Junior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Katherine Ortiz Perez"
+        meta="Junior Developer"
+        className="team-card-image"
+      />
+      <Card
+        fluid
+        image={team.placeholder}
+        header="Emma Watson"
+        meta="Junior Developer"
+        className="team-card-image"
+      />
     </>
   );
 }
